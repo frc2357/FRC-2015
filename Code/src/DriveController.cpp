d17ed73b875f41ea89f8686326561f831f022caf
Kevin Killingsworth <kevin@redfenix.com>
2015-02-07 12:52:42 -0600

    Fix the name for DriveController
    This class was mistakenly named DriverController.

---
+++ Code/src/DriveController.cpp
@@ -0,0 +1,18 @@
+/*
+ * DriverController.cpp
+ *
+ *  Created on: Feb 7, 2015
+ *      Author: Robotics
+ */
+
+#include <DriveController.h>
+
+DriveController::DriveController() {
+	// TODO Auto-generated constructor stub
+
+}
+
+DriveController::~DriveController() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ Code/src/DriveController.h
@@ -0,0 +1,17 @@
+/*
+ * DriverController.h
+ *
+ *  Created on: Feb 7, 2015
+ *      Author: Robotics
+ */
+
+#ifndef SRC_DRIVECONTROLLER_H_
+#define SRC_DRIVECONTROLLER_H_
+
+class DriveController {
+public:
+	DriveController();
+	virtual ~DriveController();
+};
+
+#endif /* SRC_DRIVECONTROLLER_H_ */
